f4b57de43ac59cc53ba6eea73b392b759b18acd6
A.M. Smith <ServiceDog@users.noreply.github.com>
2022-10-12 10:03:38 -0700

    feat(rest): add REST interfaces
    * docs(icd): initialize icd
    
    Signed-off-by: AM Smith <smitham42@gmail.com>
    
    chore(ci): provision files
    
    Signed-off-by: AM Smith <smitham42@gmail.com>

---
+++ client-grpc/src/lib.rs
@@ -0,0 +1,7 @@
+//! Exposes svc-cargo Client Functions
+
+/// Scheduler Client Library: Client Functions, Structs
+pub mod client {
+    #![allow(unused_qualifications)]
+    include!("grpc.rs");
+}
