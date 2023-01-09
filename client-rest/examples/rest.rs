d379897a02ffba1d7d4e12643c419919d3ab7741
A.M. Smith <ServiceDog@users.noreply.github.com>
2023-01-09 15:50:03 -0800

    feat: r2 api updates, autogen openapi spec
    chore: use lib-common time functions

--- client-rest/examples/rest.rs
+++ client-rest/examples/rest.rs
@@ -1,8 +1,8 @@
 //! Example communication with this service
 
+use chrono::{Duration, Utc};
 use hyper::{Body, Client, Method, Request, Response};
 use hyper::{Error, StatusCode};
-use std::time::{Duration, SystemTime};
 use svc_cargo_client_rest::types::*;
 
 fn evaluate(resp: Result<Response<Body>, Error>, expected_code: StatusCode) -> (bool, String) {
@@ -105,15 +105,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // POST /cargo/query
     {
-        let depart_timestamp_min = SystemTime::now() + Duration::from_secs(60);
+        let depart_timestamp_min = Utc::now() + Duration::seconds(60);
         let data = FlightQuery {
             // Arbitrary UUIDs
             vertiport_depart_id: "cabcdd14-03ab-4ac0-b58c-dd4175bc587e".to_string(),
             vertiport_arrive_id: "59e51ad1-d57d-4d2c-bc2d-e2387367d17f".to_string(),
-            timestamp_depart_min: Some(depart_timestamp_min),
-            timestamp_depart_max: Some(depart_timestamp_min + Duration::from_secs(360)),
-            timestamp_arrive_min: None,
-            timestamp_arrive_max: None,
+            time_depart_window: Some(TimeWindow {
+                timestamp_min: depart_timestamp_min,
+                timestamp_max: depart_timestamp_min + Duration::seconds(360),
+            }),
+            time_arrive_window: None,
             cargo_weight_kg: 1.0,
         };
         let data_str = serde_json::to_string(&data).unwrap();
